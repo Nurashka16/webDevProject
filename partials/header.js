da42f93dc31ca82b58013c8a3e65f86bf2c86121
Nura <tilepovanura@gmail>
2025-11-23 19:31:31 +0300

    #

--- partials/header.js
+++ partials/header.js
@@ -5,7 +5,7 @@ document.write(`
             <li><a href="../../js-Nurashka16/index.html">Главная</a></li>
             <li><a href="../../js-Nurashka16/lab1/index.html" >Лаба 1</a></li>
             <li><a href="../../js-Nurashka16/lab2/index.html">Лаба 2</a></li>
-            <li><a href="../../js-Nurashka16/lab3/lab3.html">Лаба 3</a></li>
+            <li><a href="../../js-Nurashka16/lab3/index.html">Лаба 3</a></li>
             <li><a href="../../js-Nurashka16/lab4/index.html">Лаба 4</a></li>
             <li><a href="../../js-Nurashka16/lab5/index.html">Лаба 5</a></li>
             <li><a href="../../js-Nurashka16/lab6/index.html">Лаба 6</a></li>
