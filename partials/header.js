b0480f039b6470378fadfb730100af331d7e9dfe
Nura <tilepovanura@gmail>
2025-12-03 14:46:09 +0300

    #

--- partials/header.js
+++ partials/header.js
@@ -2,13 +2,13 @@ document.write(`
 <header class="header">
     <nav class="nav">
         <ul class="nav__list">
-            <li><a href="../../js-Nurashka16/index.html">Главная</a></li>
-            <li><a href="../../js-Nurashka16/lab1/index.html" >Лаба 1</a></li>
-            <li><a href="../../js-Nurashka16/lab2/index.html">Лаба 2</a></li>
-            <li><a href="../../js-Nurashka16/lab3/index.html">Лаба 3</a></li>
-            <li><a href="../../js-Nurashka16/lab4/index.html">Лаба 4</a></li>
-            <li><a href="../../js-Nurashka16/lab5/index.html">Лаба 5</a></li>
-            <li><a href="../../js-Nurashka16/lab6/index.html">Лаба 6</a></li>
+            <li><a href="../index.html">Главная</a></li>
+            <li><a href="../lab1/index.html>Лаба 1</a></li>
+            <li><a href="../lab2/index.html">Лаба 2</a></li>
+            <li><a href="../lab3/index.html">Лаба 3</a></li>
+            <li><a href="../lab4/index.html">Лаба 4</a></li>
+            <li><a href="../lab5/index.html">Лаба 5</a></li>
+            <li><a href="../lab6/index.html">Лаба 6</a></li>
             <!--нужно добавить Фио по заданию, но я добавила в footer-->
         </ul>
     </nav>
